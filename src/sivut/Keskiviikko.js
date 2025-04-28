2479aea6af7c39aa4dc37f9d0bc88092a7228096
Iida Varinowski <Iida.Varinowski@kukkula.local>
2025-04-28 12:53:10 +0300

    Ensimmäinen commit

--- src/sivut/Keskiviikko.js
+++ src/sivut/Keskiviikko.js
@@ -3,8 +3,8 @@ import React from "react";
 function Maanantai() {
   return (
     <div>
-      <h2>Maanantai</h2>
-      <p>Ruokalista: Pastaa ja salaattia</p>
+      <h2>Keskiviikko</h2>
+      <p>Ruokalista: Broilerikiusausta, Kasviskiusausta, Munkki ja mehua</p>
     </div>
   );
 }
--- src/sivut/Perjantai.js
+++ src/sivut/Perjantai.js
@@ -3,7 +3,7 @@ import React from "react";
 function Maanantai() {
   return (
     <div>
-      <h2>Maanantai</h2>
+      <h2>Perjantai</h2>
       <p>Ruokalista: Pastaa ja salaattia</p>
     </div>
   );
